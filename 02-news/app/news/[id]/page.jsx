ca0d32d9778547c33c14a037b52da78b04113425
Veronica Clavijo <veuge.clavijo@gmail.com>
2026-02-19 22:43:17 -0400

    Adding new routes and nested routes.

---
+++ 02-news/app/news/[id]/page.jsx
@@ -0,0 +1,5 @@
+export default function NewDetailsPage({ params }) {
+  return (
+    <h1>New details {params.id}</h1>
+  );
+}
---
+++ 02-news/app/news/page.jsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+const news = [
+  { id: 'first-news', title: 'First new item' },
+  { id: 'second-news', title: 'Second new item' },
+  { id: 'third-news', title: 'Third new item' },
+];
+
+export default function NewsPage() {
+  return (
+    <>
+      <h1>News Page</h1>
+      {news.map((newDetails) => (
+        <Link key={newDetails.id} href={`/news/${newDetails.id}`}>
+          <p>{newDetails.title}</p>
+        </Link>
+      ))}
+    </>
+  );
+}
